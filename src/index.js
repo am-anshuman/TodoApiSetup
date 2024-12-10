2108b38d6ae3049d12551139db069d585fc659f2
am-anshuman <anshumansahu0310@gmail.com>
2024-12-10 08:30:18 +0530

    express router and code segregation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { PORT } = require('./config/server.config');
+const router = require('./routes');
 
 const app = express();
+
+app.use('/api', router); // localhost:3000/api/...
 
 app.listen(PORT, () => {
     console.log(`Server started at port: ${PORT}`);
